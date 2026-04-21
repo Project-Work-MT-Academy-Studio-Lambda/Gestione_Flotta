5ae5770357c716dee6878f59f03efafcca235a0d
Luca Atella <atellaluca@outlook.it>
2026-04-21 11:15:17 +0200

    feat: add __post_init__ validation__ to user domain

--- src/lambda-car-backend/domain/user.py
+++ src/lambda-car-backend/domain/user.py
@@ -7,3 +7,11 @@ class User:
     name: str
     email: str
     hashed_password: str
+
+    def __post_init__(self):
+        if not self.name:
+            raise ValueError("Name cannot be empty")
+        if not self.email:
+            raise ValueError("Email cannot be empty")
+        if not self.hashed_password:
+            raise ValueError("Hashed password cannot be empty")
