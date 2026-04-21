62b18f51a3a9ece3fe5fc0c9ec66dfc9766cd7b8
Luca Atella <atellaluca@outlook.it>
2026-04-21 11:19:25 +0200

    feat: add protocol logic to user repository

---
+++ src/lambda-car-backend/repositories/user_repository.py
@@ -0,0 +1,20 @@
+from typing import Protocol
+from uuid import UUID
+from domain.user import User
+
+
+class UserRepository(Protocol):
+    def get_by_id(self, user_id: UUID) -> User | None:
+        ...
+
+    def get_by_email(self, email: str) -> User | None:
+        ...
+
+    def exists_by_email(self, email: str) -> bool:
+        ...
+
+    def save(self, user: User) -> None:
+        ...
+
+    def delete(self, user_id: UUID) -> None:
+        ...
