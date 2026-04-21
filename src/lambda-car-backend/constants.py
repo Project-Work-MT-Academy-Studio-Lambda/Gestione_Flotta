0c4b4aebed9680f0b440a6c2695a140a377139f9
Luca Atella <atellaluca@outlook.it>
2026-04-21 15:49:01 +0200

    chore: add constants file

---
+++ src/lambda-car-backend/constants.py
@@ -0,0 +1,6 @@
+class Constants:
+     
+    EMAIL_ALREADY_EXISTS = "Email already exists"
+    EMAIL_ALREADY_USE = "Email already in use"
+    USER_NOT_FOUND = "User not found"
+    INVALID_CREDENTIALS = "Invalid credentials"
