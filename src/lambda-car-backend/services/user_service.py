3cb0a724e91c4180094b14c7d28f6dc928646d93
Luca Atella <atellaluca@outlook.it>
2026-04-22 14:09:34 +0200

    refactor: add _get_user_or_raise function

--- src/lambda-car-backend/services/user_service.py
+++ src/lambda-car-backend/services/user_service.py
@@ -8,11 +8,16 @@ class UserService:
     def __init__(self, user_repository: UserRepository, password_hasher: ArgonPasswordHasher):
         self.user_repository = user_repository
         self.password_hasher = password_hasher
+    
+    def _get_user_or_raise(self, user_id: UUID) -> User:
+        user = self.user_repository.get_by_id(user_id)
+        if not user:
+            raise ValueError(Constants.USER_NOT_FOUND)
+        return user
 
     def create_user(self, name: str, email: str, password: str) -> User:
         if self.user_repository.exists_by_email(email):
             raise ValueError(Constants.EMAIL_ALREADY_USE)
-        
         user = User(id=uuid4(), name=name, email=email, hashed_password=self.password_hasher.hash(password))
         self.user_repository.save(user)
         return user
@@ -24,9 +29,7 @@ class UserService:
         return self.user_repository.get_by_email(email)
     
     def update_user(self, user_id: UUID, name: str, email: str, password: str) -> User:
-        user = self.get_user(user_id)
-        if not user:
-            raise ValueError(Constants.USER_NOT_FOUND)
+        user = self._get_user_or_raise(user_id)
         if user.email != email and self.user_repository.exists_by_email(email):
             raise ValueError(Constants.EMAIL_ALREADY_USE)
         user.name = name
@@ -35,9 +38,7 @@ class UserService:
         return user
     
     def change_password(self, user_id: UUID, current_password: str, new_password: str) -> User:
-        user = self.get_user(user_id)
-        if not user:
-            raise ValueError(Constants.USER_NOT_FOUND)
+        user = self._get_user_or_raise(user_id)
         if not self.password_hasher.verify(current_password, user.hashed_password):
             raise ValueError(Constants.INVALID_CREDENTIALS)
         user.hashed_password = self.password_hasher.hash(new_password)
